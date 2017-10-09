426338cf2efa2b56062dbfd8152f070b0a55dc68
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-09 12:43:02 -0500

    routes: creates dummy routes for shops, updates templates, adds partials for header and footer

--- routes/shops.js
+++ routes/shops.js
@@ -3,7 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('shops working');
+  res.render('shops/index');
+});
+
+router.get('/new', (req, res) => {
+  res.render('shops/new');
+});
+
+router.get('/:id', (req, res) => {
+  res.render('shops/show');
+});
+
+
+router.get('/:id/edit', (req, res) => {
+  res.render('shops/edit');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 const bodyParser = require('body-parser');
 
+const ejs = require('ejs');
+
 const shops = require('./routes/shops');
 const donuts = require('./routes/donuts');
 const employees = require('./routes/employees');
@@ -14,6 +16,9 @@ const employees = require('./routes/employees');
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+
+// configure ejs for templating
+app.set('view engine', 'ejs');
 
 // middlewear routers
 app.use('/shops', shops);

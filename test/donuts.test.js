c15e637f3ab57555c9025453fe0dc86af115e056
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-11 16:12:20 -0500

    style: adds navigation and back buttons between shops and employees

--- test/donuts.test.js
+++ test/donuts.test.js
@@ -1,9 +1,9 @@
 /*jshint esversion: 6 */
 process.env.PORT = 3030;
+process.env.NODE_ENV = 'test';
 
 const expect = require('chai').expect;
 const server = require('../server');
 
 const request = require('supertest')(server);
-// const seed = require("../seeds/states_cities").seed;
 const knex = require('../db/knex');

bfe84692318f4f700e78db90ab49e36dbdcf7459
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-12 13:39:35 -0500

    refactor: moves migrations and seeds to db folder, adds production env to knex"

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,31 @@
+const path = require('path');
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost:5432/donuttycoon_dev'
+    connection: 'postgres://localhost:5432/donuttycoon_dev',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   },
 
   test: {
     client: 'pg',
-    connection: 'postgres://localhost:5432/donuttycoon_test'
+    connection: 'postgres://localhost:5432/donuttycoon_test',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
+
 };

28b8fbe28c4f98c71eeb5b6c8d8ec4b7ee7c68f1
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-10-12 13:45:17 -0500

    db: adds relative route for production migrations and seeds

--- knexfile.js
+++ knexfile.js
@@ -26,6 +26,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
+  },
+  migrations: {
+    directory: path.join(__dirname, 'db', 'migrations')
+  },
+  seeds: {
+    directory: path.join(__dirname, 'db', 'seeds')
   }
-
 };
